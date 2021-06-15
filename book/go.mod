4fccf27f658cf0d6287251c1d92c93682ad4f05e
kaneziki <51665048+xuanxianyou@users.noreply.github.com>
2021-06-15 10:49:25 +0800

    Add files via upload

---
+++ book/go.mod
@@ -0,0 +1,17 @@
+module GoProject/WebProject/MicroService/UserBook/book
+
+go 1.16
+
+require (
+	github.com/PuerkitoBio/goquery v1.6.1
+	github.com/antchfx/htmlquery v1.2.3
+	github.com/chromedp/chromedp v0.7.3
+	github.com/go-kit/kit v0.10.0
+	github.com/google/uuid v1.0.0
+	github.com/gorilla/mux v1.7.3
+	github.com/hashicorp/consul/api v1.3.0
+	github.com/jinzhu/gorm v1.9.16
+	go.mongodb.org/mongo-driver v1.5.3
+	golang.org/x/sys v0.0.0-20210608053332-aa57babbf139 // indirect
+	gopkg.in/yaml.v2 v2.4.0
+)
--- book/main.go
+++ book/main.go
@@ -1,1 +1,59 @@
+package main
 
+import (
+	"GoProject/WebProject/MicroService/UserBook/book/consul"
+	"GoProject/WebProject/MicroService/UserBook/book/endpoints"
+	"GoProject/WebProject/MicroService/UserBook/book/services"
+	"GoProject/WebProject/MicroService/UserBook/book/transports"
+	"context"
+	"flag"
+	"fmt"
+	"github.com/google/uuid"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+)
+
+func main(){
+	// 服务注册基本信息
+	consulHost := flag.String("consul.host", "localhost", "consul address")
+	consulPort := flag.Int("consul.port", 8500, "consul port")
+	name := flag.String("service.name", "book", "service name")
+	address := flag.String("service.address", "localhost", "service address")
+	port := flag.Int("service.port", 8000, "service port")
+	tag := flag.String("service.tag", "primary", "service tag")
+	flag.Parse()
+
+	Id := *name + "-" +  uuid.New().String()
+	// 生成consul客户端实例
+	consulAddress := fmt.Sprintf("%s:%d", *consulHost, *consulPort)
+	client := consul.NewClient(consulAddress)
+	ctx := context.Background()
+	errChan := make(chan error)
+	// book 服务
+	bookService := services.BookService{}
+
+	bookEndpoints :=&endpoints.BookEndpoints{
+		GetBookEndpoint: endpoints.MakeGetBookEndpoint(bookService),
+	}
+
+	r := transports.MakeHttpHandler(ctx, bookEndpoints)
+
+	// 启动并注册服务
+	go func() {
+		client.RegisterService(Id, *name, *address, *port, *tag)
+		errChan <- http.ListenAndServe(":"+strconv.Itoa(*port), r)
+	}()
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
+	// 收集错误，注销服务
+	err := <-errChan
+	client.UnRegisterService(Id)
+	log.Fatal(err)
+}
